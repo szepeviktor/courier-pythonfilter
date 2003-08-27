7379eb7150976e7f72a27bd376e19071c6eb045b
Gordon Messmer <gordon@dragonsdawn.net>
2003-08-27 20:10:30 +0000

    Improvements to the build system.

--- setup.py
+++ setup.py
@@ -1,15 +1,37 @@
 #!/usr/bin/python
 
+import glob
+import os
+import sys
 from distutils.core import setup
 
+
+
+def enum_filters():
+    filters = glob.glob(os.path.join( 'filters', '*.py' ))
+    argi = 0
+    while argi < len(sys.argv):
+        if sys.argv[argi] == '--without-filter':
+            # Save the excluded filter arg, and delete both args from argv
+            exclude = sys.argv[argi+1]
+            del sys.argv[argi:argi+2]
+            try:
+                del filters[ filters.index(os.path.join('filters',exclude)) ]
+            except:
+                print exclude, 'not found in filters'
+        else:
+            argi = argi + 1
+    return filters
+
+
+
 setup(name="courier-pythonfilter",
-      version="0.1",
+      version="0.2",
       description="Python filtering architecture for the Courier MTA.",
       author="Gordon Messmer",
       author_email="gordon@dragonsdawn.net",
       url="http://www.dragonsdawn.net/~gordon/courier-patches/courier-pythonfilter/",
       scripts=['pythonfilter'],
       packages=['courier'],
-      data_files=[('lib/pythonfilter', ['filters/dialback.py',
-                                        'filters/ratelimit.py'])]
+      data_files=[('lib/pythonfilter', enum_filters())]
      )
