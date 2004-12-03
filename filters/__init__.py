84bf7569a59319e70703d72eb0f5e3b623fcc501
Gordon Messmer <gordon@dragonsdawn.net>
2004-12-03 04:15:37 +0000

    Always distribute all modules.  Load only modules indicated by configuration file.

---
+++ filters/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+import sys
+
+sys.stderr.write( 'Import individual modules rather than the "pythonfilter" container.\n' )
--- setup.py
+++ setup.py
@@ -5,34 +5,15 @@ import os
 import sys
 from distutils.core import setup
 
-
-
-def enum_filters():
-    filters = glob.glob(os.path.join( 'filters', '*.py' ))
-    argi = 0
-    while argi < len(sys.argv):
-        if sys.argv[argi] == '--without-filter':
-            # Save the excluded filter arg, and delete both args from argv
-            exclude = sys.argv[argi+1]
-            del sys.argv[argi:argi+2]
-            try:
-                del filters[ filters.index(os.path.join('filters',exclude)) ]
-            except:
-                print exclude, 'not found in filters'
-        else:
-            argi = argi + 1
-    return filters
-
-
-
 setup(name="courier-pythonfilter",
-      version="0.4",
+      version="0.7",
       description="Python filtering architecture for the Courier MTA.",
       author="Gordon Messmer",
       author_email="gordon@dragonsdawn.net",
       url="http://www.dragonsdawn.net/~gordon/courier-patches/courier-pythonfilter/",
       license="GPL",
       scripts=['pythonfilter'],
-      packages=['courier'],
-      data_files=[('lib/pythonfilter', enum_filters())]
+      packages=['courier', 'pythonfilter'],
+      package_dir = {'pythonfilter': 'filters'},
+      data_files=[('/etc/', ['pythonfilter.conf'])]
      )
