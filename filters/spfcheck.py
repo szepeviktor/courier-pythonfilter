3da7c3e7ffc589407af96d64b7030ab4cfc03a84
Gordon Messmer <gordon@dragonsdawn.net>
2008-03-23 08:06:05 +0000

    Update copyright information.

--- filters/spfcheck.py
+++ filters/spfcheck.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python
 # spfcheck -- Courier filter which checks SPF records using the "spf" module
-# Copyright (C) 2004  Jon Nelson <jnelson@jamponi.net>
+# Copyright (C) 2004-2008  Jon Nelson <jnelson@jamponi.net>
 #
-# This program is free software; you can redistribute it and/or modify
+# This file is part of pythonfilter.
+#
+# pythonfilter is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
+# pythonfilter is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-# License: GPL v2
+# along with pythonfilter.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
 import courier.control
