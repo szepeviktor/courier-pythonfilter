eb655883fd6b484250f317a46e0b2d5480262702
Gordon Messmer <gordon@dragonsdawn.net>
2012-09-09 19:34:08 -0700

    Release 1.8

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ import sys
 from distutils.core import setup
 
 setup(name="courier-pythonfilter",
-      version="1.7",
+      version="1.8",
       description="Python filtering architecture for the Courier MTA.",
       author="Gordon Messmer",
       author_email="gordon@dragonsdawn.net",
       url="http://www.dragonsdawn.net/~gordon/courier-pythonfilter/",
       license="GPL",
-      scripts=['pythonfilter', 'pythonfilter-quarantine'],
+      scripts=['pythonfilter', 'pythonfilter-quarantine', 'dropmsg'],
       packages=['courier', 'pythonfilter'],
       package_dir={'pythonfilter': 'filters'},
       data_files=[('/etc/', ['pythonfilter.conf',
