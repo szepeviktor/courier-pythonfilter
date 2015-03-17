211febb870dfca54f3f5790f15d5f87d67ea2911
Gordon Messmer <gordon@dragonsdawn.net>
2015-03-17 14:10:42 -0700

    Release 1.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 from distutils.core import setup
 
 setup(name="courier-pythonfilter",
-      version="1.8",
+      version="1.9",
       description="Python filtering architecture for the Courier MTA.",
       author="Gordon Messmer",
       author_email="gordon@dragonsdawn.net",
